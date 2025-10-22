f539e5934dbf24b22107028cc7251790dfb29064
Maksym Polishchuk <mpolishchuck@vu.cdu.edu.ua>
2025-10-23 00:25:48 +0300

    Add simple_form.py

---
+++ python/simple_form.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    tpl_data = {}
+    if request.method == 'POST':
+        tpl_data['username'] = request.form['username']
+        tpl_data['email'] = request.form['email']
+    return render_template('form.html.j2', **tpl_data)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
