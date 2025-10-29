16ef7fa538b04846ec69f3051d9510aabba5ba52
Maksym Polishchuk <mpolishchuck@vu.cdu.edu.ua>
2025-10-29 23:23:39 +0200

    Update db_dml.py

--- python/db_dml.py
+++ python/db_dml.py
@@ -1,7 +1,8 @@
 def insert():
     db = get_db()
-    db.execute("INSERT INTO messages (author, content) VALUES (?, ?)", (author, content))
+    cursor = db.execute("INSERT INTO messages (author, content) VALUES (?, ?)", (author, content))
     db.commit()
+    return cursor.lastrowid
 
 def select():
     db = get_db()
@@ -10,10 +11,12 @@ def select():
 
 def update():
     db = get_db()
-    db.execute("UPDATE messages SET content = ? WHERE id = ?", (new_text, msg_id))
+    cursor = db.execute("UPDATE messages SET content = ? WHERE id = ?", (new_text, msg_id))
     db.commit()
+    return cursor.rowcount
 
 def delete():
     db = get_db()
-    db.execute("DELETE FROM messages WHERE id = ?", (msg_id))
+    cursor = db.execute("DELETE FROM messages WHERE id = ?", (msg_id))
     db.commit()
+    return cursor.rowcount
